a596cfa712f97203ad01a53800262211b315454a
Jaby24-dtk <office.ibgct@gmail.com>
2026-07-09 15:42:48 +0800

    Add Google Analytics component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter_Tight, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -37,6 +38,8 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
+        
+        <GoogleAnalytics gaId="G-E1Y1GRKG2M" />
       </body>
     </html>
   );
